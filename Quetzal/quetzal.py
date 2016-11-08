51a4a8eb6ebffd3066ce752a91e6c8078f03d29a
Guardado <jgarzagu@ucsd.esu>
2016-11-07 18:02:03 -0800

    Fixed problem with error placing components from origin

--- Quetzal/quetzal.py
+++ Quetzal/quetzal.py
@@ -23,7 +23,7 @@ from descartes import PolygonPatch
 
 smoothness=0
 
-# Plot Colors
+## Plot Colors
 RED = "#ff0000"
 GREEN = "#00ff00"
 BLACK = "#000000"
@@ -33,7 +33,7 @@ BLUE = "#0000ff"
 YELLOW = "#ff00ff"
 PURPLE = "#ff00ff"
 
-# Board layers for plotting
+## Board layers for plotting
 ALL_LAYERS = ["Dimension", "Holes", "Top", "Bottom", "onBoard", "offBoard", "tKeepout", "bKeepout"]
 ALL_COMPONENTS = ["Dimension", "Holes", "onBoard", "offBoard", "tKeepout", "bKeepout"]
 
@@ -63,25 +63,25 @@ def extract_components(element):
     return shapely.ops.unary_union([holes, on_board, tKeepout, bKeepout])
 
 
-# Prints locked and unlocked elements dictionaries together with the board dimension
-# (e.g  print_board_elements(board, locked_elements, unlocked_elements))
+## Prints locked and unlocked elements dictionaries together with the board dimension
+## (e.g  print_board_elements(board, locked_elements, unlocked_elements))
 def print_board_elements (brd, elementone, elementtwo):
     from matplotlib import pyplot as plt
 
-    # Extract Polygons
+    ## Extract Polygons
     brdDimension = extract_board(brd)
 
-    # Create plot
+    ## Create plot
     fig = plt.figure(2, figsize=(10,10), dpi=90)
     subfig = fig.add_subplot(111)
 
-    # Add Multi-polygons of the element to plot
+    ## Add Multi-polygons of the element to plot
 
-    # Add board polygons
+    ## Add board polygons
     patch = PolygonPatch(brdDimension, facecolor=GREEN, edgecolor=BLACK, alpha=0.5, zorder=2)
     subfig.add_patch(patch)
 
-    # Add element one polygons
+    ## Add element one polygons
     for e, polygon in elementone.iteritems():
         if (polygon.geom_type == 'Polygon'):
             patch = PolygonPatch(polygon, facecolor=BLUE, edgecolor=BLACK, alpha=0.5, zorder=2)
@@ -91,7 +91,7 @@ def print_board_elements (brd, elementone, elementtwo):
                 patch = PolygonPatch(single, facecolor=BLUE, edgecolor=BLACK, alpha=0.5, zorder=2)
                 subfig.add_patch(patch)
 
-    # Add element two polygons
+    ## Add element two polygons
     for e, polygon in elementtwo.iteritems():
         if (polygon.geom_type == 'Polygon'):
             patch = PolygonPatch(polygon, facecolor=RED, edgecolor=BLACK, alpha=0.5, zorder=2)
@@ -101,12 +101,12 @@ def print_board_elements (brd, elementone, elementtwo):
                 patch = PolygonPatch(single, facecolor=RED, edgecolor=BLACK, alpha=0.5, zorder=2)
                 subfig.add_patch(patch)
 
-    # Get board width and height
+    ## Get board width and height
     bounds = brdDimension.bounds
     width  = abs(bounds[2] - bounds[0])
     height = abs(bounds[3] - bounds[1])
 
-    # Plot Board
+    ## Plot Board
 
     subfig.set_title('PCB')
     xrange = [bounds[0] - 0.1*width,  bounds[2] + 0.1*width]
@@ -123,19 +123,20 @@ def print_board_elements (brd, elementone, elementtwo):
 
     return
 
-# Prints given board layers in an array 
-# (e.g  print_board_layers (brd, ["Dimensions", "tKeepout"])) 
+
+## Prints given board layers in an array 
+## (e.g  print_board_layers (brd, ["Dimensions", "tKeepout"])) 
 def print_board_layers (brd, layers):
     from matplotlib import pyplot as plt
 
-    # Extract Polygons
+    ## Extract Polygons
     brdDimension = extract_board(brd)
 
-    # Create plot
+    ## Create plot
     fig = plt.figure(2, figsize=(10,10), dpi=90)
     subfig = fig.add_subplot(111)
 
-    # Add Multi-polygons to plot
+    ## Add Multi-polygons to plot
 
     for layer in layers:
         if layer == "Dimension":
@@ -186,7 +187,7 @@ def print_board_layers (brd, layers):
         else:
             continue
 
-    # Plot Board
+    ## Plot Board
     
     bounds = brdDimension.bounds
     width  = abs(bounds[2] - bounds[0])
@@ -208,120 +209,78 @@ def print_board_layers (brd, layers):
     return 
 
 
-# This function should be called before starting to move components.
-# It checks if the unlocked elements doesn't have any overlapping 
-# with locked elements or off-Board errors. Then this function passes
-# any unlocked elements without errors to the locked elements dictionary 
-def unlocked_to_locked (board, locked_elements, unlocked_elements):
-
-    # Returning variable (1 if board has overlapping problems: 0 if Not)
-    ret = 0
-
-    # Copy dictionaries
-    cpy_unlocked_elements = unlocked_elements.copy()
-    cpy_locked_elements = locked_elements.copy()
-
-    # Make an off-Board Polygon with the difference between a big polygon and the board size
-    universe = Polygon([(-1000,-1000),(1000,-1000),(1000,1000),(-1000,1000)])
-    un_board = universe.difference(board);
-
-    # Check if each locked_element overlaps with a unlocked_element
-    for unlock_element, unlock_polygon in cpy_unlocked_elements.iteritems():
-
-        # print "Checking unlocked component: " + unlock_element.get_name()
-        e1 = { unlock_element : unlock_polygon } 
-        error = 0
-
-        ## Check if a part of the component is off-Board 
-        offb = e1[unlock_element].intersection(un_board)
-        if offb:
-            # print ("A portion of Part {} is off the board".format(unlock_element.get_name()))
-            error = 1
-
-        ## Check that the component is on the Board.  
-        for lock_element, lock_polygon in cpy_locked_elements.iteritems():
-            e2 = { lock_element : lock_polygon }
-
-            ## Check that the component doesn't overlap for top and bottom. 
-            overlap = e1[unlock_element].intersection(e2[lock_element])
-            if overlap:
-                # print ("Part {} overlaps Part {}".format
-                #     (unlock_element.get_name(),
-                #      lock_element.get_name()))
-                error = 1
-
-        if (not error):
-            # Move unlocked_element to locked_element dictionary
-            # if unlocked_element doesn't overlap
-            locked_elements.update(e1)
-            unlocked_elements.pop(unlock_element, None)
-        else:
-            ret = 1
-
-    return ret
-
-
-# Check if any element in the dictionary of unlocked_elements overlaps
-# with any element of the dictionary with locked_elements 
+## Check if any element in the dictionary of unlocked_elements overlaps
+## with any element of the dictionary with locked_elements 
 def check_unlocked_components (board, locked_elements, unlocked_elements):
 
-    # Copy dictionaries
-    cpy_unlocked_elements = unlocked_elements.copy()
-    cpy_locked_elements = locked_elements.copy()
+	## Make an off-Board Polygon with the difference between a big polygon and the board size
+	universe = Polygon([(-1000,-1000),(1000,-1000),(1000,1000),(-1000,1000)])
+	un_board = universe.difference(board);
 
-    # Make an off-Board Polygon with the difference between a big polygon and the board size
-    universe = Polygon([(-1000,-1000),(1000,-1000),(1000,1000),(-1000,1000)])
-    un_board = universe.difference(board);
 
-    # Check if each locked_element overlaps with a unlocked_element
-    for unlock_element, unlock_polygon in cpy_unlocked_elements.iteritems():
+	## While the unlocked_elements dictionary is NOT empty 
+	while (bool(unlocked_elements) == True):
 
-        e1 = { unlock_element : unlock_polygon } 
+		## Check if each locked_element overlaps with a unlocked_element
+		dictonary_item = unlocked_elements.iteritems().next()
+		unlock_element = dictonary_item[0]
+		unlock_polygon = dictonary_item[1]
+		#for unlock_element, unlock_polygon in unlocked_elements.iteritems():
 
-        ## Check if a part of the component is off-Board 
-        offb = e1[unlock_element].intersection(un_board)
-        if offb:
-            # print ("A portion of Part {} is off the board".format(unlock_element.get_name()))
-            return unlock_element
+		e1 = { unlock_element : unlock_polygon } 
 
-        ## Check that the component is on the Board.  
-        for lock_element, lock_polygon in cpy_locked_elements.iteritems():
-            e2 = { lock_element : lock_polygon }
+		## Check if a part of the component is off-Board 
+		offb = e1[unlock_element].intersection(un_board)
+		if offb:
+		    # print ("A portion of Part {} is off the board".format(unlock_element.get_name()))
+		    return unlock_element
 
-            ## Check that the component doesn't overlap for top and bottom. 
-            overlap = e1[unlock_element].intersection(e2[lock_element])
-            if overlap:
-                # print ("Part {} overlaps Part {}".format
-                #     (unlock_element.get_name(),
-                #      lock_element.get_name()))
-                return unlock_element
+		## Check that the component is on the Board.  
+		for lock_element, lock_polygon in locked_elements.iteritems():
+		    e2 = { lock_element : lock_polygon }
 
-        # Move unlocked_element to locked_element dictionary
-        # if unlocked_element doesn't overlap
-        locked_elements.update(e1)
-        unlocked_elements.pop(unlock_element, None)
+		    ## Check that the component doesn't overlap for top and bottom. 
+		    overlap = e1[unlock_element].intersection(e2[lock_element])
+		    if overlap:
+		        # print ("Part {} overlaps Part {}".format
+		        #     (unlock_element.get_name(),
+		        #      lock_element.get_name()))
+		        return unlock_element
 
-    return None
+		## Move unlocked_element to locked_element dictionary
+		## if unlocked_element doesn't overlap
+		locked_elements.update(e1)
+		unlocked_elements.pop(unlock_element, None)
+
+	return None
 
 
 
 def main():
 
+	## Quetzal command line Arguments
     parser = argparse.ArgumentParser(description="This tool automatically places unlocked elements \
                                     (unplaced components) of a Eagle .brd file inside the board.")
     parser.add_argument("-i","--inbrd",
                         help="The .brd file with unlocked elements to be placed.",
                         metavar="<unplaced_board>.brd",
                         required=True)
-    parser.add_argument("-o","--outbrd",help="Name of output .brd file",metavar="<output>.brd")
+    parser.add_argument("-o","--outbrd",
+    					help="Name of output .brd file",
+    					metavar="<output>.brd")
+    parser.add_argument("-d","--display",
+    					help="Displays the boards and its elements before and after placing",
+    					action='store_true', dest='display')
     args = parser.parse_args()
 
-    
+
+    ## Open the board and check that the file exists and
+    ## is of type (*brd)
     try:
         ## ShapelySwoop Open Board
         brd = ShapelySwoop.open(args.inbrd)
 
-        ## Extract elements
+        ## Extract elements8
         board = extract_board(brd)
         elements = brd.get_elements()
         pass
@@ -342,21 +301,18 @@ def main():
     unlocked_elements = {}
     for e in elements:
         if (e.get_locked() == 1):
-            #  Locked element
+            ## Locked element
             locked_elements.update({e: extract_components(e)})
         else:
-            # Unlocked element
+            ## Unlocked element
             unlocked_elements.update({e: extract_components(e)})
 
-    # TODO: Add this with display option
-    # print_board_elements(brd, locked_elements, unlocked_elements)
 
+    ## Displays the board in a plot with locked elements in blue and 
+    ## unlocked elements in orange **before** stating to place. 
+    if (args.display):
+    	print_board_elements(brd, locked_elements, unlocked_elements)
 
-    # Checks if any unlocked component can be passed to the locked dictionary. 
-    ret = unlocked_to_locked(board, locked_elements, unlocked_elements)
-    if (ret == 0):
-        print ("No parts overlap or are off-Board")
-        sys.exit(0)
 
     ## Get board dimensions
     board_min_x = board.bounds[0]
@@ -364,22 +320,20 @@ def main():
     board_max_x = board.bounds[2]
     board_max_y = board.bounds[3]
 
-    ## Move component
+
+    ## Move elements (components)
     element_x = 0
     element_y = 0
     BOARD_CLEARANCE = 1.5
     move_dictionary = {} # Type {element.get_name(): [moves_x, moves_y, width, height]}
     board_pass = 0
     while (board_pass == 0):
-
         ## Check if element overlaps or is off Board
         element = check_unlocked_components(board, locked_elements, unlocked_elements)
-
         if (element == None):
             print ("Finish moving overlapped or off-Board elements")
             board_pass = 1
         else:
-
             ## Check if unlocked element is already in move_dictionary
             if element.get_name() in move_dictionary:
 
@@ -441,16 +395,20 @@ def main():
             #END = time.time()
             #print "Exec Time 3 = {}".format(END - START)
 
-    # TODO: Add this with display option
-    # print_board_elements(brd, locked_elements, unlocked_elements)
-    # print_board_layers(brd, ALL_LAYERS)
 
-    # Write Output board file
+    ## Displays the board in a plot with locked elements in blue and 
+    ## unlocked elements in orange **after** placing. 
+    if (args.display):
+    	print_board_elements(brd, locked_elements, unlocked_elements)
+    #print_board_layers(brd, ALL_LAYERS)
+
+
+    ## Write Output board file
     if (args.outbrd):
         brd.write(args.outbrd)
 
 
-    # Exit
+    ## Exit
     sys.exit(0)
 
 
